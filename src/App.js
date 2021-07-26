b579b2e93f19b651f72e4dc8c90260bc40c66678
blingblin-g <luvbliny@gmail.com>
2021-07-27 03:07:58 +0900

    Add infinity loop
    무한 스크롤 기능을 추가했습니다.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,61 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import Comments from "./Comments";
 
 const URL = `https://jsonplaceholder.typicode.com/comments`;
-let PAGE = 1;
 const LIMIT = 10;
 
 function App() {
   const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const setDataApi = async () => {
-      const commentData = await getCommentAPI();
-      setData(commentData);
-      console.log(commentData);
-    };
-    setDataApi();
-  }, []);
+  const isEnd = useRef(false);
+  const page = useRef(1);
+  const loadRef = useRef();
 
   const getCommentAPI = async () => {
     try {
-      const response = await fetch(`${URL}?_page=${PAGE}&_limit=${LIMIT}`);
+      const response = await fetch(
+        `${URL}?_page=${page.current}&_limit=${LIMIT}`
+      );
       const comments = await response.json();
-
+      page.current += 1;
+      console.log(comments);
       return comments;
     } catch (e) {
       console.log(`error: ${e}`);
     }
   };
 
+  const setDataApi = useCallback(async () => {
+    const commentData = await getCommentAPI();
+    if (commentData.length === 0) {
+      isEnd.current = true;
+    }
+    setData((data) => [...data, ...commentData]);
+  }, []);
+
+  const observerHandler = ([entry]) => {
+    if (entry.isIntersecting && isEnd.current !== true) {
+      setDataApi();
+    }
+  };
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0,
+    };
+    const observer = new IntersectionObserver(observerHandler, options);
+
+    observer.observe(loadRef.current);
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <div className="App">
       {data.map((x) => (
         <Comments key={x.id} id={x.id} email={x.email} comment={x.body} />
       ))}
+      <div className="crash" ref={loadRef} />
     </div>
   );
 }
