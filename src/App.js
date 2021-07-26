0f82bcb9f349ecb0a3be127536c057b250fada68
blingblin-g <luvbliny@gmail.com>
2021-07-27 01:32:19 +0900

    Add get API
    API data를 받아왔습니다

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import React, { useState, useEffect } from "react";
+import Comments from "./Comments";
+
+const URL = `https://jsonplaceholder.typicode.com/comments`;
+let PAGE = 1;
+const LIMIT = 10;
+
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const setDataApi = async () => {
+      const commentData = await getCommentAPI();
+      setData(commentData);
+      console.log(commentData);
+    };
+    setDataApi();
+  }, []);
+
+  const getCommentAPI = async () => {
+    try {
+      const response = await fetch(`${URL}?_page=${PAGE}&_limit=${LIMIT}`);
+      const comments = await response.json();
+
+      return comments;
+    } catch (e) {
+      console.log(`error: ${e}`);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      {data.map((x) => (
+        <Comments key={x.id} id={x.id} email={x.email} comment={x.body} />
+      ))}
     </div>
   );
 }
